d27b24b5ff64e47e7836ab299f382a70d95f91dc
Nil Xie <bfgh2678@gmail.com>
2024-04-25 23:32:02 +0800

    fix: create gemini-pro-vision component

--- apps/generative-ai/src/app/remote-entry/components/gemini-pro-vision/gemini-pro-vision.component.ts
+++ apps/generative-ai/src/app/remote-entry/components/gemini-pro-vision/gemini-pro-vision.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { GenerativeAiService } from '../../services/generative-ai.service';
+import { FileConversionService } from '../../services/file-conversion.service';
 
 @Component({
   selector: 'app-gemini-pro-vision',
   templateUrl: './gemini-pro-vision.component.html',
   styleUrl: './gemini-pro-vision.component.scss',
 })
-export class GeminiProVisionComponent {}
+export class GeminiProVisionComponent {
+  isLoading = false;
+  isBlocked = false;
+
+  prompt = this.formBuilder.control('', Validators.required);
+  promptText = '';
+
+  imageFile: File | undefined;
+  preview = '';
+  responseText = '';
+
+  constructor(
+    public formBuilder: FormBuilder,
+    private cdRef: ChangeDetectorRef,
+    private generativeAiService: GenerativeAiService,
+    private fileConversionService: FileConversionService
+  ) { }
+}
